c25a2044185f4686c9f2c390479831f27d307ad1
esellers25 <erin.e.sellers@gmail.com>
2021-03-18 18:00:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    const greeting = document.querySelector("#text")
+    greeting.innerHTML = "This is really cool!"
+    console.log("The DOM has loaded");
+});
